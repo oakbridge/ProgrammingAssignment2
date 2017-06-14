dd590355a17ffca20c61dd81ce3827da6da46d87
OakBridge <joao.ponte@fe.up.pt>
2017-06-14 22:49:32 +0200

    Programming assignment 2 attempt

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+# This function creates a list of functions
+# that allow us to get, set and store a matrix
+# and its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  cachedInverse <- NULL
+  if (!is.matrix(x)) {
+    x <- NULL
+  }
+  set <- function(newMatrix){
+    if(!is.matrix(newMatrix)) {
+      x <<- NULL
+    } else {
+      x <<- newMatrix 
+    }
+    cachedInverse <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverseMatrix){
+    if(!is.matrix(inverseMatrix)) {
+      cachedInverse <<- NULL
+    } else {
+      cachedInverse <<- inverseMatrix 
+    }
+  }
+  getInverse <- function(){
+    return(cachedInverse)
+  }
+  
+  list(set = set, 
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
 
+# The cacheSolve gets as parameter x which is a 
+# result of the makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverseMatrix <- x$getInverse()
+  if(!is.null(inverseMatrix)) {
+    message("Returning cached value")
+    return(inverseMatrix)
+  }
+  data <- x$get()
+  inverseMatrix <- tryCatch ({
+    inverseMatrix <- solve(data, ...)
+  }, error = function(e) {
+    NULL
+  }, warning = function(e) {
+    NULL
+  })
+  x$setInverse(inverseMatrix)
+  inverseMatrix
 }
